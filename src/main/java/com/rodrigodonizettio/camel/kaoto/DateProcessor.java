f56ee3640a7170bc6c9d269c8feb86e79073c6c9
rodrigodonizettio <rodrigodonizettio@gmail.com>
2025-08-18 08:41:30 -0300

    Adding Camel .yaml Route for File Watching and Custom Bean for Date Format

---
+++ src/main/java/com/rodrigodonizettio/camel/kaoto/DateProcessor.java
@@ -0,0 +1,28 @@
+package com.rodrigodonizettio.camel.kaoto;
+
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+
+@ApplicationScoped
+@Named("dateProcessor")
+public class DateProcessor implements Processor {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    private final SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Long fileEventDate = exchange.getIn().getHeader("CamelFileLastModified", Long.class);
+
+        Date eventDate = new Date(fileEventDate);
+        
+        exchange.getIn().setHeader("CamelFileLastModified", formatter.format(eventDate));
+    } 
+}
